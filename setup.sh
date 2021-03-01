9095919895d1d191e5da33e3cb28253f815a4422
Hanna Maria Borecka-Bielska <hanna.maria.borecka@cern.ch>
2021-03-01 07:16:07 -0500

    Update lcg from 92 to 94 to enable root 6.14

--- setup.sh
+++ setup.sh
@@ -4,15 +4,17 @@ export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 . /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
 if [[ $HOSTNAME == *"lps.umontreal.ca"* ]];
 then
-    . $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh "lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt ROOT" \
-	"lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt root_numpy" \
-	"lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt tensorflow" \
-	"lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt keras" \
-	"lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt pandas" \
-	"lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt Jinja2" \
-	"lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt matplotlib" \
-	"lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt h5py" \
-	"lcgenv -p LCG_92python3 x86_64-slc6-gcc62-opt scikitlearn" 
+    . $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh "lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt ROOT" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt root_numpy" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt tensorflow" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt keras" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt keras_applications" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt keras_preprocessing" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt pandas" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt Jinja2" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt matplotlib" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt h5py" \
+	"lcgenv -p LCG_94python3 x86_64-slc6-gcc62-opt scikitlearn" 
 else
     . $ATLAS_LOCAL_ROOT_BASE/packageSetups/localSetup.sh "lcgenv -p LCG_97python3 x86_64-centos7-gcc8-opt ROOT" \
         "lcgenv -p LCG_97python3 x86_64-centos7-gcc8-opt root_numpy" \
